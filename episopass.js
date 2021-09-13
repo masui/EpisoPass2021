952c761dc7756a9b5508a6e398444131627f74ac
Toshiyuki Masui <masui@pitecan.com>
2021-09-13 19:23:38 +0900

    x で削除

--- episopass.js
+++ episopass.js
@@ -12,29 +12,40 @@ let data = [
     '沖縄'
 ]
 
+function clickn(n){
+    return function(){
+	data.splice(n,1)
+	initdata()
+    }
+}
+
 function initdata(){
     let form = $('#answers')
     form.children().remove()
-
+    
     for(var i=0;i<data.length;i++){
-      let span = $('<span>')
-      let input = $('<input>')
-          .attr('type','text')
-          .attr('class','answerinput')
-          .val(data[i])
-      span.append(input)
-      let a = $('<a>')
-         .attr('href','xxxx')
-         .text('✖')
-      let checkspan = $('<span>')
-        .attr('class','check')
-      checkspan.append(a)
-
-      //check = $('<span>')
-      //check.append(a)
-      //check.attr('class','check')
-      //span.append(check)
-      span.append(checkspan)
-      form.append(span)
+	let span = $('<span>')
+	let input = $('<input>')
+            .attr('type','text')
+            .attr('class','answerinput')
+            .val(data[i])
+	span.append(input)
+	let a = $('<span>')
+            .text('✖')
+	    .attr('class','check')
+	a.on('click',clickn(i))
+	     
+	/*
+	  let checkspan = $('<span>')
+          .attr('class','check')
+	  checkspan.append(a)
+	*/
+	
+	//check = $('<span>')
+	//check.append(a)
+	//check.attr('class','check')
+	//span.append(check)
+	span.append(a)
+	form.append(span)
     }
 }
