09ebe1149a5112cf7d953bcc0f6705bdb139954f
Toshiyuki Masui <masui@pitecan.com>
2021-09-13 12:55:46 +0900

    ...

--- episopass.js
+++ episopass.js
@@ -6,7 +6,14 @@ let data = [
 function initdata(){
     form = $('#answers')
     form.children().remove()
+
     span = $('<span>')
-    span.text('xxxxxxx')
+    span.css('padding','10px')
+    input = $('<input>')
+    input.attr('type','text')
+    input.val('東京')
+    span.append(input)
+    check = $('<span><a href="xxx">✖</a></span>')
+    span.append(check)
     form.append(span)
 }
