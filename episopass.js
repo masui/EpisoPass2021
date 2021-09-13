38aaca145a520c032babdd7d0ef70d1f1a0f79a2
Toshiyuki Masui <masui@pitecan.com>
2021-09-14 07:09:21 +0900

    リファクタリング

--- episopass.js
+++ episopass.js
@@ -12,55 +12,68 @@ let 回答リスト = [
     '沖縄'
 ]
 
-function n個目の答を削除する関数(n){
+function n個目の答を削除する関数(n,リスト){
     return function(){
-	回答リスト.splice(n,1)
-	回答リスト表示()
+	リスト.splice(n,1)
+	リスト表示(リスト)
     }
 }
 
-function n個目の答を登録する関数(n){
+function n個目の答を登録する関数(n,リスト){
     return function(e){
-	回答リスト[n] = e.target.value
-	回答リスト表示()
+	リスト[n] = e.target.value
+	リスト表示(リスト)
     }
 }
 
 function 重複と空エントリを削除(リスト){
-    let 新しいリスト = []
+    let 新リスト = []
     for(var i=0;i<リスト.length;i++){
 	let 項目 = リスト[i]
-	if(項目 != '' && ! 新しいリスト.includes(項目)){
-	    新しいリスト.push(項目)
+	if(項目 != '' && ! 新リスト.includes(項目)){
+	    新リスト.push(項目)
 	}
     }
     リスト.length = 0 // 配列を削除
-    for(var i=0;i<新しいリスト.length;i++){
-	リスト[i] = 新しいリスト[i]
+    for(var i=0;i<新リスト.length;i++){
+	リスト[i] = 新リスト[i]
     }
 }
 
-function 回答リスト表示(){
-    重複と空エントリを削除(回答リスト)
-    let 回答リストdiv = $('#answers')
-    回答リストdiv.children().remove()
+function リスト表示(リスト){
+    重複と空エントリを削除(リスト)
+    let リストdiv = $('#answers')
+    リストdiv.children().remove()
     
-    for(var i=0;i<回答リスト.length;i++){
-	let 答エントリ = $('<span>')
+    for(var i=0;i<リスト.length;i++){
+	let エントリ = $('<span>')
 
 	$('<input>')
             .attr('type','text')
             .attr('class','answerinput')
-            .val(回答リスト[i])
-	    .appendTo(答エントリ)
-	    .on('change',n個目の答を登録する関数(i))
+            .val(リスト[i])
+	    .appendTo(エントリ)
+	    .on('change',n個目の答を登録する関数(i,リスト))
 	
 	$('<span>')
 	    .text('✖')
 	    .attr('class','check')
-	    .on('click',n個目の答を削除する関数(i))
-	    .appendTo(答エントリ)
+	    .on('click',n個目の答を削除する関数(i,リスト))
+	    .appendTo(エントリ)
 
-	回答リストdiv.append(答エントリ)
+	リストdiv.append(エントリ)
     }
+
+    let エントリ = $('<span>')
+
+    $('<input>')
+        .attr('type','text')
+        .attr('class','answerinput')
+        .attr('placeholder','(追加)')
+	.appendTo(エントリ)
+	.on('change',n個目の答を登録する関数(i,リスト))
+
+    リストdiv.append(エントリ)
 }
+
+
