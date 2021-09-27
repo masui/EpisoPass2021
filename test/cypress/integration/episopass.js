e967d4602606986444804897fcf3541eb0a5d7e0
Toshiyuki Masui <masui@pitecan.com>
2021-09-27 09:41:08 +0900

    ...

--- test/cypress/integration/episopass.js
+++ test/cypress/integration/episopass.js
@@ -2,8 +2,8 @@
 // EpisoPass.comのテスト
 //
 
-const URL = 'https://EpisoPass.com/index.html'
-// const URL = 'http://pitecan.com/tmp/episopass.html'
+const URL = 'https://EpisoPass.com/index.html' // デプロイ環境
+// const URL = 'http://pitecan.com/tmp/episopass.html' // テスト環境
 
 describe('EpisoPassのテスト', () => {
     it('EpisoPass.comにアクセスしてデフォルト問題をチェック', () => {
@@ -33,11 +33,7 @@ describe('EpisoPassのテスト', () => {
     it('長い問題のテスト', () => {
 	cy.visit(encodeURI(URL+'?questions=ビフテキをおかわりしたのは?;洋菓子屋でオナラするのは?;吊橋が怖いのは?;意外とカツカレーが美味かったのは?;デカい石が割れてるのは?;麻雀させられたのは?;Google道案内に騙されたのは?;K君が川に落ちるのは?;マサコの家は?;包丁売ってたのは?&answers=北海道;青森;岩手;宮城;秋田;山形;福島;茨城;栃木;群馬;埼玉;千葉;東京;神奈川;新潟;富山;石川;福井;山梨;長野;岐阜;静岡;愛知;三重;滋賀;京都;大阪;兵庫;奈良;和歌山;鳥取;島根;岡山;広島;山口;徳島;香川;愛媛;高知;福岡;佐賀;長崎;熊本;大分;宮崎;鹿児島;沖縄'))
 
-	// 何故か動かない...
-	//cy.get('#episodbbutton').click()
-	//cy.get('#name0').should('have.value', '北海道')
-	
-	cy.get('#editbutton').click()
+	cy.get('#editbutton').click() // 問題編集画面に移動
 	cy.get('#question0').should('have.value', 'ビフテキをおかわりしたのは?')
 	cy.get('#answer0-0').should('have.value', '北海道')
 	cy.get('#answer0-46').should('have.value', '沖縄')
@@ -45,7 +41,7 @@ describe('EpisoPassのテスト', () => {
 	cy.get('#answer1-0').should('have.value', '北海道')
 	cy.get('#answer1-46').should('have.value', '沖縄')
 
-	cy.get('#dasbutton').click()
+	cy.get('#dasbutton').click() // DAS生成画面に移動
 
 	// 左上のボタンを10回クリック
 	for(var i=0;i<10;i++){
